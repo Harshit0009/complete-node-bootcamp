03b05ef6339c9ecc420555737efa57deb9a509fa
Harshit Tiwari <tiwariharshit909@gmail.com>
2022-12-24 20:52:25 +0530

    node farm and section 6 section 5 remaining

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 //////////////////////////////////
 // file read and write
 
@@ -32,21 +34,48 @@ const url = require("url");
 //////////////////////////////////
 // SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
+
+// in arrow function if there is no curly braces, then output gests automatically returned
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCards = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
+
+  // console.log(req.url);
+  //console.log(url.parse(req.url, true)); // we are parsing that id into the object form and true is used to give object
+  const { query, pathname} = url.parse(req.url, true); // this is the es6 structuring where we pass the same variable name as in object and thus the value will get set to same as in the object.
   const pathName = req.url;
 
+  // Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW."); // creating the path for the overview page.
-  } else if (pathName === "/product") {
-    res.end("This is PRODUCT");
+    res.writeHead(200, {'content-type': 'text/html'});
+    const cardHtml = dataObj.map(el => replaceTemplate(tempCards, el)).join(''); // this join function will join all the data received by map function in the array into an empty string.
+    // cards html has now become a string so we can operate it now a string but it is only a single string.
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
+    res.end(output); // creating the path for the overview page.
+
+    //Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {'content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    //API
   } else if (pathName === "/api") {
     // fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8")
     res.writeHead(200, {'content-type': 'application/json'});
     res.end(data);
+
+    //OTHER PAGE
   } else {
     res.writeHead(404, {
       // creating the object for the header
---
+++ 1-node-farm/starter/modules/replaceTemplate.js
@@ -0,0 +1,13 @@
+module.exports = (temp, product) => { // g means global which means select all appearances of this variable
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);  // we added "//" and g so that we can select all occurrences of PRODUCTNAME
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+  }
